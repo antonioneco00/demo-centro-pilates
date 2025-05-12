b88c07623abcb4bff8effc702fcb2f29dea42054
Antonio <antonionc2000@hotmail.es>
2025-05-12 13:42:44 +0200

    fix: update base URL in sitemap for correct deployment link

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://pilatesvitalia.com";
+  const baseUrl = "https://pilates-vitalia.vercel.app";
 
   return [
     {
